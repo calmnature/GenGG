b4aa2a473f57b9fc79793317a7d1af99d695dfd3
BestDeveloper <calmnature2@gmail.com>
2025-10-23 16:15:51 +0900

    Settings : Init

---
+++ src/main/java/geng/your/gg/geng/GenGApplication.java
@@ -0,0 +1,13 @@
+package geng.your.gg.geng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenGApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenGApplication.class, args);
+    }
+
+}
---
+++ src/test/java/geng/your/gg/geng/GenGApplicationTests.java
@@ -0,0 +1,13 @@
+package geng.your.gg.geng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenGApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
